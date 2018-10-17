80267029044fb7270a42e902873030afc088427b
VJefimov <44232324+VJefimov@users.noreply.github.com>
2018-10-17 16:52:43 +0300

    Update esimene_ülesanne_VJ.py

---
+++ esimene_ülesanne_VJ.py
@@ -0,0 +1,33 @@
+def name(summa_arv):
+    i=1
+    summa_arv=1
+    sisend=int(input("arv"))
+    while i<sisend:
+        '''tsukel arvutab sisestatud numbri selle väiksemate täisnumbrite summa'''
+        i+=1
+        summa_arv=summa_arv+i
+    return summa_arv
+
+print(name("summa_arv"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+           
+	
+
